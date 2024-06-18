91a304483f2e6bad20e6168bd53ec865d6bfbbc0
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-06-18 14:10:19 +0200

    rename

--- scripts.js
+++ scripts.js
@@ -70,7 +70,7 @@ function loadMap() {
 
 function fetchGitHubPhotos() {
 	const xhr = new XMLHttpRequest();
-	const url = "https://api.github.com/repos/icoegi/2023-Torino/contents/"
+	const url = "https://api.github.com/repos/icoegi/2023-torino/contents/"
 	xhr.open('GET', url, true);
 	xhr.onload = function () {
 		const data = JSON.parse(this.response);
